2a43ad48ec87f75916a0918f42e00cd6ebe0a5c4
Mykhailo Kikabidze <kikabidzemisha@gmail.com>
2024-03-10 19:18:01 +0100

    Add the connection between REST API and PostgreSQL

---
+++ backend/sql_app/crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+import asyncio
+
+
+async def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+async def create_user(db: Session, user: schemas.User):
+    password = user.password
+    db_user = models.User(login=user.login, email=user.email, hashed_password=password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
---
+++ backend/sql_app/main.py
@@ -0,0 +1,34 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi import Depends, FastAPI, HTTPException
+import asyncio
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+
+# Dependency
+async def get_db_session() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
+
+
+@app.get("/users/{user_email}", response_model=schemas.User)
+async def read_user(user_email: str, db: AsyncSession = Depends(get_db_session)):
+    db_user = crud.get_user_by_email(db, email=user_email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/", response_model=schemas.User)
+async def create_user(user: schemas.User, db: AsyncSession = Depends(get_db_session)):
+    db_user = await crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return await crud.create_user(db=db, user=user)
