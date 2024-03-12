f35752ce9f35ea8596874d2766a312564c9a3c16
Mykhailo Kikabidze <kikabidzemisha@gmail.com>
2024-03-12 22:51:04 +0100

    Final version

--- backend/sql_app/crud.py
+++ backend/sql_app/crud.py
@@ -9,14 +9,14 @@ from sqlalchemy.orm import selectinload
 async def get_user(db: AsyncSession, email: str, password: str):
     async with db as session:
         result = await session.execute(
-            select(models.User).filter(models.User.email == email, models.User.hashed_password == password)
+            select(models.User).filter(models.User.email == email, models.User.password == password)
         )
         return result.scalars().first()
 
 
 async def create_user_db(db: AsyncSession, user: schemas.User):
     password = user.password
-    db_user = models.User(login=user.login, email=user.email, hashed_password=password)
+    db_user = models.User(login=user.login, email=user.email, password=password)
     async with db as session:
         session.add(db_user)
         await session.commit()
